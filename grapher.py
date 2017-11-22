eedb1220130c67566110ba3cecb380ac36dd35bd
oyho <oyvind.hovdsveen@nordicsemi.no>
2017-11-22 09:22:16 +0100

    Cleaning up grapher.py

--- grapher.py
+++ grapher.py
@@ -5,48 +5,49 @@ import re
 import math
 import matplotlib.pyplot as plt
 
+#Generating two smoothed curves as well, each of these are in minutes to smooth over
 smoothingtime1 = 20
 smoothingtime2 = 60
-with open(sys.argv[1], 'r') as csvfile:
+
+#use
+#  nrfjprog --memrd <address> -n <bytes>
+#and write it to a file, pass that file as argument:
+#  python3 grapher.py memrd.log
+with open(sys.argv[1], 'r') as memdumpfile:
     plt.figure(1)
-    lines = list(csv.reader(csvfile, delimiter=' '))
+    lines = list(csv.reader(memdumpfile, delimiter=' '))
     values = []
-    smoothed_15mins = []
-    smoothed_hr = []
+    smoothed_1 = []
+    smoothed_2 = []
     timestamps_hr = []
     timestamp_min = 0
     i = 0
     for line in lines:
         for element in range(1,5):
-            timestamp_min += 1
             if line[element] == 'FFFFFFFF':
                 continue
             if line[element] == '00000000':
                 continue
 
+            timestamp_min += 1
             value = int(line[element], 16)
-            values.append((value*60.0)/1000) #convert to avg kW
+            values.append((value*60.0)/1000) #convert to avg kW, assuming 1000 blinks/kWh
             timestamps_hr.append(timestamp_min/60.0)
 
-            #+1 since i is included with +1 below
-            start = i - smoothingtime1 + 1
-            if i < (smoothingtime1-1):
-                start = 0
-
-            smoothed_15mins.append(sum(values[start:i+1])/smoothingtime1)
-
-            #+1 since i is included with +1 below
-            start = i - smoothingtime2 + 1
-            if i < (smoothingtime2-1):
-                start = 0
-
-            smoothed_hr.append(sum(values[start:i+1])/smoothingtime2)
+            #Calculate smoothed curves
             i += 1
+            start = i - smoothingtime1
+            if start >= 0:
+                smoothed_1.append(sum(values[start:i])/smoothingtime1)
+
+            start = i - smoothingtime2
+            if start >= 0:
+                smoothed_2.append(sum(values[start:i])/smoothingtime2)
+
 
     plt.plot(timestamps_hr, values)
-    plt.plot(timestamps_hr, smoothed_15mins)
-    plt.plot(timestamps_hr, smoothed_hr)
-    plt.legend()
+    plt.plot(timestamps_hr[smoothingtime1-1:], smoothed_1)
+    plt.plot(timestamps_hr[smoothingtime2-1:], smoothed_2)
     plt.ylabel('kW')
     plt.xlabel('Time')
     plt.show()
